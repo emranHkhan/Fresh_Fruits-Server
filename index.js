46720bf3f022815f3378e1eb15d62cc711c9e2e1
Emran H Khan <emran.khan2400@gmail.com>
2021-04-06 02:29:45 +0600

    adding comments

--- index.js
+++ index.js
@@ -19,16 +19,17 @@ client.connect(err => {
   const fruitsCollection = client.db("fuitsdb").collection("fruits");
   const allOrders = client.db("allitemsdb").collection("items");
 
+  // adding products(fruits) to db
   app.post('/addProduct', (req, res) => {
     const newEvent = req.body;
     console.log(newEvent);
     fruitsCollection.insertOne(newEvent)
       .then(result => {
-        console.log('count', result.insertedCount);
         res.send(result.insertedCount > 0)
       })
   })
 
+  // showing products to Home page
   app.get('/products', (req, res) => {
     fruitsCollection.find()
       .toArray((err, items) => {
@@ -36,9 +37,9 @@ client.connect(err => {
       })
   })
 
+  //saving the ordered product to db
   app.post('/checkout', (req, res) => {
     const checkoutInfo = req.body;
-    console.log('connected');
     allOrders.insertOne(checkoutInfo)
       .then(result => {
         res.send(result.insertedCount > 0)
@@ -47,37 +48,36 @@ client.connect(err => {
 
   })
 
+  //finding all the products for respective email
   app.get('/orders', (req, res) => {
-    console.log(req.query.email)
+    
     allOrders.find({ email: req.query.email })
       .toArray((err, documents) => {
         res.send(documents)
       })
   })
 
+  //showing all the products for respective email
   app.get('/allorders', (req, res) => {
     fruitsCollection.find({})
       .toArray((err, result) => {
-        console.log(result);
-        console.log(err);
         res.send(result)
       })
   })
 
+  //delete product by its id
   app.delete('/delete/:id', (req, res) => {
-    console.log(req.params.id);
     fruitsCollection.deleteOne({ _id: ObjectId(req.params.id) })
       .then(result => {
-        console.log(result);
         res.send(result.deletedCount > 0)
 
       })
   })
 
+  //delete ordered product by its id
   app.delete('/remove/:id', (req, res) => {
     allOrders.deleteOne({ _id: ObjectId(req.params.id) })
       .then(result => {
-        console.log(result);
         res.send(result.deletedCount > 0)
       })
   })
