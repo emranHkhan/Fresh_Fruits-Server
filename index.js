eeeca798ebab2457721ee0089abbca501055f6b0
Emran H Khan <emran.khan2400@gmail.com>
2021-04-05 20:08:19 +0600

    port fixed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
-// const port = 5000;
+const port = 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vllde.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
